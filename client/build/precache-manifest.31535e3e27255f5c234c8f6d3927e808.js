fa25fc20c008b77021376072f80af45f42a5ad60
sylendran <sylendra@blockchainappfactory.com>
2019-10-16 20:26:54 +0530

    complete create token

---
+++ client/build/precache-manifest.31535e3e27255f5c234c8f6d3927e808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e4e73c83e92c3fd47c2a8a6c8295ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2b5a01c673f054ccdaf",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "661bc8ca27048d6fdf85",
+    "url": "/static/js/2.a7a861a6.chunk.js"
+  },
+  {
+    "revision": "d2b5a01c673f054ccdaf",
+    "url": "/static/js/main.03cc3ee7.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
